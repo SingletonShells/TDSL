6039b10ce99a91b66e4814b9d705095ad8d5ac1b
SingletonShells <singletonshells@gmail.com>
2026-05-18 12:20:40 +0200

    First Commit

---
+++ teedeezel-test-runner/src/main/java/com/singletonshells/teedeezel/App.java
@@ -0,0 +1,13 @@
+package com.singletonshells.teedeezel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
